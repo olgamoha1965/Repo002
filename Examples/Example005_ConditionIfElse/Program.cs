8ce13944524c40c757b5aa1d55e6153e2e0ffaf0
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-01 18:07:01 +0300

    Exsample005_Condition if else

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);    
+}
+*/
+
+/*
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //принимает любое написание имени Маша
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);    
+}
+*/
