b71939fba804677c2a2404c8e7734c03607554f4
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-01 18:02:15 +0300

    Exsample003_Sum

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿/*int numberA = 31;
+int numberB =5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
+
+
+int numberA = new Random().Next(1, 10);  // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+
+/*int numberA = new Random().Next(1, 10);
+
+int numberB = new Random().Next(1, 10);
+
+int result = numberA + numberB;
+Console.Write($"{numberA} + {numberB} = {result}");
+*/
