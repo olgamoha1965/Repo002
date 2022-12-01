f4e7e082f111adb071f16ad5e437ad87d2544d54
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-01 18:21:05 +0300

    Example011_Array Library

---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,84 @@
+﻿//Создание массива случайным способом и его написание
+
+/*void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+*/
+
+//Решение задач при помощи массива
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+
+array[4] = 4; //добавим принудительно 4 (чтобы были в массиве)
+array[6] = 4;
+
+PrintArray(array);
+
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
