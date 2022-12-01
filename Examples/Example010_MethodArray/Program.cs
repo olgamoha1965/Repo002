2d41dc449ccafeb87d225fdbbab0ecee3c54f248
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-01 18:18:47 +0300

    Example010_Method Array

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,38 @@
+﻿//Метод определяет индекс числа 18 (в данном случае их два)
+
+/*int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+
+int find = 18;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1;
+    index++;
+}
+*/
+
+//Метод определяет индекс числа 18 (в данном случае после первого числа поиск завершается)
+
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+
+int find = 18;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
