3cd324b7467ca400670b3205ab6d60c88adf8ddb
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-05 10:03:22 +0300

    Example015

---
+++ Examples/Example015/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+
+Console.WriteLine(text);               
+
+var data = text.Split(" ")
+
+                .Select(item => item.Split(","))
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                .Where(e => e.x % 2 == 0)
+                .Select(point => (point.x * 10, point.y + 10))
+                .ToArray();
+
+for(int i = 0; i < data.Length; i++)
+{
+    //Console.WriteLine(data[i].Item1 * 10);
+    //Console.WriteLine(data[i].x * 10);
+    Console.WriteLine(data[i]);
+        
+/*
+    for(int k = 0; k < data[i].Length; k++)
+    {
+        Console.WriteLine(data[i][k]);
+    }
+    */
+    //Console.WriteLine();
+}
